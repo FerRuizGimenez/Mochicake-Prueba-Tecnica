affbce434db2799c0e2d5626c1d4b31f7bd53940
Fernando Ruiz Gimenez <48037548+FerRuizGimenez@users.noreply.github.com>
2026-05-21 16:06:28 +0200

    PlayerController script
    - The player can move
    - Change the direction of the player based on player input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    bool isMovingRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Move();
+        CheckInput();
+    }
+
+    void Move()
+    {
+        transform.position += transform.right * moveSpeed * Time.deltaTime;
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ChangeDirection();
+        }    
+    }
+    void ChangeDirection()
+    {
+        if (isMovingRight)
+        {
+            isMovingRight = false;
+            transform.rotation = Quaternion.Euler(0,-90,0);
+        }
+        else
+        {
+            isMovingRight = true;
+            transform.rotation = Quaternion.Euler(0,0,0);
+        }
+    }
+}
